d2b7d2b8566275ec1329096d9516d8cda4b2abeb
Ashish Singh <ashusingh15082001@gmail.com>
2022-07-16 00:09:07 +0530

    Update multerConfigure.js

--- utilities/multerConfigure.js
+++ utilities/multerConfigure.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const path = require("path");
 
 const fileFilter = function (req, file, cb) {
   switch (file.mimetype) {
@@ -14,7 +15,13 @@ const fileFilter = function (req, file, cb) {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/images");
+    const destinationAbsolutePath = path.join(
+      __dirname,
+      "../",
+      "uploads",
+      "images"
+    );
+    cb(null, destinationAbsolutePath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
